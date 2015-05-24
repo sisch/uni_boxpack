086d95b7e32f7e42319236a6205e4d0639731922
sisch <sisch105@hhu.de>
2015-05-24 11:26:46 +0200

    Bare program

--- boxpack.c
+++ boxpack.c
@@ -1,3 +1,58 @@
-int main(){
+// Declare global variables
+
+// ---INLINE ASSEMBLER PART---
+// (Part of the task!!!)
+// inlineAddition returns the sum of two integers a and b
+// TODO: Error handling depending on flags
+int inlineAddition(int a, int b){
+  return 4;
+}
+// inlineSubtraction returns the result of a - b
+int inlineSubtraction(int a, int b){
+  return 4;
+}
+
+// ---FITTING ALGORITHMS---
+// First-Fit
+void first_fit(){
+  
+}
+// Best-Fit
+void best_fit(){
+  
+}
+// Next-Fit
+void next_fit(){
+  
+}
+// Almost-Worst-Fit
+void almost_worst_fit(){
+  
+}
+
+// ---IO FUNCTIONS---
+// writeOutput prints a correctly formatted output line
+// per container to stdout
+void writeOutput(){
+
+}
+
+// readInput reads two lines
+// will call createContainer n times during the first line
+// will call fillContainer m times during second line
+// TODO: Handle input format errors
+void readInput(){
+
+}
+
+void init(){
+
+}
+int close(){
   return 0;
+}
+// Program entry
+int main(){
+  init();
+  return close();
 }
